6d39b2004a128006937dbc9bf40c654b73d2e4e3
Toonen, Rens <renstoonen1@hotmail.com>
2015-11-12 14:38:48 +0100

    test added

---
+++ test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+
+describe("test wrapper", function(){
+    describe("testerino", function(){
+      it('should do stuff', function(){
+        var i = 1;
+        expect(i).to.equal(2);
+      });
+    });
+
+});
