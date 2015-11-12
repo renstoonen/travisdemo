c7de044bae2af7a1d311a340fa1cf74b9105037c
Toonen, Rens <renstoonen1@hotmail.com>
2015-11-12 16:45:19 +0100

    test should pass

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ describe("test wrapper", function(){
     describe("testerino", function(){
       it('should do stuff', function(){
         var i = 1;
-        expect(i).to.equal(12333);
+        expect(i).to.equal(1);
       });
     });
 
