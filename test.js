cd7d7d8d6de7872f512ca5afc0e4c86be2efddc4
Toonen, Rens <renstoonen1@hotmail.com>
2015-11-13 10:15:05 +0100

    gaat dit werken????

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ var expect = require('chai').expect;
 describe("test wrapper", function(){
     describe("testerino", function(){
       it('should do stuff', function(){
-        var i = 2;
-        expect(i).to.equal(2);
+        var i = 3;
+        expect(i).to.equal(3);
       });
     });
 
