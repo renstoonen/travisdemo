d1c0d11b9cf8e806c25e26ae1235619d49247c79
Toonen, Rens <renstoonen1@hotmail.com>
2015-11-13 10:31:54 +0100

    3@#24

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ var expect = require('chai').expect;
 describe("test wrapper", function(){
     describe("testerino", function(){
       it('should do stuff', function(){
-        var i = 4;
-        expect(i).to.equal(4);
+        var i = 5;
+        expect(i).to.equal(5);
       });
     });
 
